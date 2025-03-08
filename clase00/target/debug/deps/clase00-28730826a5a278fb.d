ffa4d6ff1efa5be531e19e055d39897540a6c33e
chris <christian.rojas7740@alumnos.udg.mx>
2025-03-08 16:52:01 -0600

    LOL

---
+++ clase00/target/debug/deps/clase00-28730826a5a278fb.d
@@ -0,0 +1,5 @@
+C:\Users\chris\Desktop\PROG.DE-SIS.-AVANZADOS\clase00\target\debug\deps\libclase00-28730826a5a278fb.rmeta: src\main.rs
+
+C:\Users\chris\Desktop\PROG.DE-SIS.-AVANZADOS\clase00\target\debug\deps\clase00-28730826a5a278fb.d: src\main.rs
+
+src\main.rs:
---
+++ clase00/target/debug/deps/clase00-441f37ef8c18787d.d
@@ -0,0 +1,5 @@
+C:\Users\chris\Desktop\PROG.DE-SIS.-AVANZADOS\clase00\target\debug\deps\libclase00-441f37ef8c18787d.rmeta: src\main.rs
+
+C:\Users\chris\Desktop\PROG.DE-SIS.-AVANZADOS\clase00\target\debug\deps\clase00-441f37ef8c18787d.d: src\main.rs
+
+src\main.rs:
